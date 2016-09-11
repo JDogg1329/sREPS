add3aedcca6789e56b93034036371840693cf523
Chris <JDogg1329@gmail.com>
2016-09-11 14:22:56 +1000

    Added dbconn.php
    Attempting to neaten up code

--- addrecord.php
+++ addrecord.php
@@ -1,27 +1,16 @@
 <?php
 session_start();
-	$host = "localhost";
-	$user = "root"; // your user name
-	$pwd = ""; // your password
-	$sql_db = "srepsdb"; // your database
 
-	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-	
-	//store session to php variable
-	$user_email = $_SESSION['email'];
-	//store current date to variable
-	$currentdate = date('Y-m-d');
-	//store POST array to variable
-	$quantity = $_POST["item_quantity"];
+	require("dbconn.php");
 
-	if (!$conn)
-	{
-		// Displays an error message
-		echo "<p>Database connection failure</p>"; // not in production script
-	}
-	else
-	{
-		//query
+
+		//store session to php variable
+		$user_email = $_SESSION['email'];
+		//store current date to variable
+		$currentdate = date('Y-m-d');
+		//store POST array to variable
+		$quantity = $_POST["item_quantity"];
+
 		$query = "insert into sale (sale_id, user_email, date, quantity)
 				value (NULL, '$user_email', '$currentdate', '$quantity')";
 
@@ -39,5 +28,4 @@ session_start();
 		}
 
 		mysqli_close($conn);
-	}
 ?>
---
+++ dbconn.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "localhost";
+	$user = "root"; // your user name
+	$pwd = ""; // your password
+	$sql_db = "srepsdb"; // your database
+
+	//connecting to database
+	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+	if (!$conn)
+	{
+		// Displays an error message
+		echo "<p>Database connection failure</p>"; // not in production script
+	}
+?>
